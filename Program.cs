8fc9f50a9d9820a98171066ef74a1c43e84dbc42
mstaubs <marcus.staubs@gmail.com>
2024-06-07 17:13:27 -0400

    Switch to controllers from razor pages

--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
+using GameCollectionTracker.Data;
+using GameCollectionTracker.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IEFUserStorageRepo, EFUserStorageRepo>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddDbContext<GameContext>();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
+app.UseCors(policy => policy.AllowAnyMethod());
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
