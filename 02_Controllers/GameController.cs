8abb0b0f801222661339baddc5d93b93f3188991
mstaubs <marcus.staubs@gmail.com>
2024-06-07 15:12:26 -0400

    added some basics to flesh things out

--- 02_Controllers/GameController.cs
+++ 02_Controllers/GameController.cs
@@ -1,3 +1,18 @@
 
+using GameCollectionTracker.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameCollectionTracker.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GameController : Controller
+{
+    private readonly GameContext _gameContext;
+
+    public GameController(GameContext gameContext)
+    {
+        _gameContext = gameContext;
+    }
+
+}
--- 02_Controllers/UserController.cs
+++ 02_Controllers/UserController.cs
@@ -1,3 +1,17 @@
-
+using GameCollectionTracker.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameCollectionTracker.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : Controller
+{
+    private readonly GameContext _gameContext;
+
+    public UserController(GameContext gameContext)
+    {
+        _gameContext = gameContext;
+    }
+
+}
